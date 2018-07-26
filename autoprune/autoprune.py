dd8f33fee5358461d167db616351a893f1d59d28
kportalz <kportalz@protonmail.com>
2018-07-26 15:43:01 -0700

    maybe this time
    possibly

--- autoprune/autoprune.py
+++ autoprune/autoprune.py
@@ -8,14 +8,25 @@ class autoprune:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def prune(self):
-        if ctx.message.author.server_permissions.administrator:
-            msg = "You're an admin {0.author.mention}".format(ctx.message)
-            await client.send_message(ctx.message.channel, msg)
-        else:
-            msg = "You're an average joe {0.author.mention}".format(ctx.message)
-            await client.send_message(ctx.message.channel, msg)
+    @commands.has_permissions(kick_members=True)
+    @commands.command(pass_context=True,name="estimateprune")
+    async def estprune(self, days=30):
+        """Estimate count of members that would be pruned based on the amount of days. Staff only."""
+        if days > 30:
+            await
+            self.bot.say("Maximum 30 days")
+            return
+        if days < 1:
+            await
+            self.bot.say("Minimum 1 day")
+            return
+        msg = await
+        self.bot.say("I'm figuring this out!".format(self.bot.server.name))
+        count = await
+        self.bot.estimate_pruned_members(server=self.bot.server, days=days)
+        await
+        self.bot.edit_message(msg, "{:,} members inactive for {} day(s) would be kicked from {}!".format(count, days,
+                                                                                                         self.bot.server.name))
 
 
 def setup(bot):
